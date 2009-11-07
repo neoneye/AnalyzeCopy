fc938f83496c3e2d5026ca95c191e369a311685e
Simon Strandgaard <neoneye@opcoders.com>
2009-11-07 20:39:34 +0100

    Added test for make sure that the xattr order is preserved

--- Tests/40_extended_attributes.rb
+++ Tests/40_extended_attributes.rb
@@ -17,40 +17,40 @@ def version
 end
 
 def create
+  `touch xattr0.txt`
+  `#{PATH} -w com.opcoders.empty_xattr "" xattr0.txt`
+
   `touch xattr1.txt`
   `#{PATH} -w com.opcoders.analyzecopy_text "hello file world" xattr1.txt`
 
   `mkdir xattr2.dir`
   `#{PATH} -w com.opcoders.analyzecopy_text "hello dir world" xattr2.dir`
-
-  `touch xattr3.txt`
-  `#{PATH} -w com.opcoders.empty_xattr "" xattr3.txt`
 end
 
 def verify(original_dir)
   errors = %w(X Y Z A B C)
-  if FileTest.exist?("xattr1.txt")
+  if FileTest.exist?("xattr0.txt")
     errors.delete('X')
+    s = `#{PATH} xattr0.txt`
+    if s =~ /^com.opcoders.empty_xattr$/
+      errors.delete('A')
+    end
+  end
+  if FileTest.exist?("xattr1.txt")
+    errors.delete('Y')
     s = `#{PATH} -p com.opcoders.analyzecopy_text xattr1.txt`
     if $?.exitstatus != 0
       puts "failed reading xattr"
     elsif s =~ /^hello file world$/
-      errors.delete('A')
+      errors.delete('B')
     end
   end
   if FileTest.exist?("xattr2.dir")
-    errors.delete('Y')
+    errors.delete('Z')
     s = `#{PATH} -p com.opcoders.analyzecopy_text xattr2.dir`
     if $?.exitstatus != 0
       puts "failed reading xattr"
     elsif s =~ /^hello dir world$/
-      errors.delete('B')
-    end
-  end
-  if FileTest.exist?("xattr3.txt")
-    errors.delete('Z')
-    s = `#{PATH} xattr3.txt`
-    if s =~ /^com.opcoders.empty_xattr$/
       errors.delete('C')
     end
   end
---
+++ Tests/41_xattr_ordering.rb
@@ -0,0 +1,44 @@
+PATH = "/usr/bin/xattr"
+
+def is_installed
+  File.exists?(PATH)
+end
+
+def version
+  nil # xattr is versionless
+end
+
+def create
+  `touch myfile`
+  `#{PATH} -w com.opcoders.a_first "first" myfile`
+  `#{PATH} -w com.opcoders.b_second "second" myfile`
+  `#{PATH} -w com.opcoders.c_empty "" myfile`
+  `#{PATH} -w com.opcoders.d_last "last" myfile`
+
+  `mkdir mydir`
+  `#{PATH} -w com.opcoders.a_first "first" mydir`
+  `#{PATH} -w com.opcoders.b_second "second" mydir`
+  `#{PATH} -w com.opcoders.c_empty "" mydir`
+  `#{PATH} -w com.opcoders.d_last "last" mydir`
+end
+
+def verify(original_dir)
+  errors = %w(X Y A B)
+  if FileTest.exist?("myfile")
+    errors.delete('X')
+    s = `#{PATH} myfile`
+    # see if ordering is preserved
+    if s =~ /a_first.*b_second.*c_empty.*d_last/m
+      errors.delete('A')
+    end
+  end
+  if FileTest.exist?("mydir")
+    errors.delete('Y')
+    s = `#{PATH} mydir`
+    # see if ordering is preserved
+    if s =~ /a_first.*b_second.*c_empty.*d_last/m
+      errors.delete('B')
+    end
+  end
+  { :errors => errors }
+end
