a1ad4370a6a8a1c539fde64143e02f3f3b974f4c
Simon Strandgaard <neoneye@opcoders.com>
2009-11-07 19:25:45 +0100

    folders can be invisible

--- Tests/30_hidden.rb
+++ Tests/30_hidden.rb
@@ -15,20 +15,27 @@ def create
   FileUtils.touch("invisible.txt")
   `SetFile -a V invisible.txt`
 
+  `mkdir invisible.dir`
+  `SetFile -a V invisible.dir`
+
   FileUtils.touch("hidden.txt")
   `chflags hidden hidden.txt`
 end
 
 def verify(original_dir)
-  errors = %w(X Y Z A B)
+  errors = %w(X Y Z W A B C)
   errors.delete('X') if FileTest.exist?(".dotfile.txt")
-  if FileTest.exist?("invisible.txt")
+  if FileTest.exist?("invisible.dir")
     errors.delete('Y')
-    errors.delete('A') if `GetFileInfo -av invisible.txt` =~ /^1$/
+    errors.delete('A') if `GetFileInfo -av invisible.dir` =~ /^1$/
+  end
+  if FileTest.exist?("invisible.txt")
+    errors.delete('Z')
+    errors.delete('B') if `GetFileInfo -av invisible.txt` =~ /^1$/
   end
   if FileTest.exist?("hidden.txt")
-    errors.delete('Z')
-    errors.delete('B') if `ls -lO` =~ /^.*hidden.*hidden.txt$/
+    errors.delete('W')
+    errors.delete('C') if `ls -lO` =~ /^.*hidden.*hidden.txt$/
   end
   { :errors => errors }
 end
