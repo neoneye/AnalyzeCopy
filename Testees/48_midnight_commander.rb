5a154374f00898b55ca8bed89aa4c8f5a5e52fbf
Simon Strandgaard <neoneye@Simon-Strandgaards-Mac-mini-3.local>
2010-07-26 06:00:59 +0200

    actually good old midnight commander is able to copy hardlinked dirs. nice

---
+++ Testees/48_midnight_commander.rb
@@ -0,0 +1,25 @@
+PATH = "/opt/local/bin/mc"
+
+def is_installed
+  File.exists?(PATH)
+end
+
+def version
+  `#{PATH} --version`.entries[0].strip
+end
+
+def print_full_version
+  puts "todo"
+end
+
+def copy_data(source_dir, dest_dir)
+  ENV["DEST_DIR"] = dest_dir
+  s = <<CMD
+mkdir "$DEST_DIR"
+cd "$DEST_DIR"
+echo "You must interactively copy the files using GNU Midnight Commander" >> README.txt
+CMD
+  puts s
+  puts "------------"
+  system s
+end
