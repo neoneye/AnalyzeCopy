6a60878432865a4fba48ee2e916425b9fffdfc43
Simon Strandgaard <neoneye@mini.local>
2010-09-06 23:56:27 +0200

    learned about CpMac and made a quick test of it.. not good

---
+++ Testees/06_cpmac.rb
@@ -0,0 +1,33 @@
+#
+# If I use the -p option for preserving metadata then I get an error,
+# so I don't use the -p option in this test.
+#
+# prompt> sudo CpMac -p -r group0 /Volumes/AnalyzeCopyDest/06_cpmac/result
+# could not preserve the destination modification or creation date (-35)
+#
+# If anyone manage to get the -p option to work, let me know
+#
+PATH = "/usr/bin/CpMac"
+
+def is_installed
+  File.exists?(PATH)
+end
+
+def version
+  "versionless"
+end
+
+def print_full_version
+  puts "versionless"
+end
+
+def copy_data(source_dir, dest_dir)
+  ENV["DEST_DIR"] = dest_dir
+  ENV["SOURCE_DIR"] = source_dir
+  s = <<CMD
+sudo #{PATH} -r "$SOURCE_DIR" "$DEST_DIR"
+CMD
+  puts s
+  puts "------------"
+  system s
+end
